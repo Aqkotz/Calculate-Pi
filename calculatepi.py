beaad085079300a5dc2e2a1e744b25c6bf8951c7
Aqkotz <andy.lyme@mac.com>
2016-09-19 13:19:38 -0400

    Updated from Brython Server: September 19, 2016 1:19:38 PM EDT

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
 Author: Andy Kotz
-Credit: none
+Credit: Mr. Dennison
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
